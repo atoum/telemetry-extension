e84ac906866e0c5c3c8ba1a9e57c7667bdd0a6ba
jubianchi <contact@jubianchi.fr>
2017-10-03 00:07:22 +0200

    Add php-cs-fixer and StyleCI

--- classes/extension.php
+++ classes/extension.php
@@ -4,71 +4,70 @@ namespace mageekguy\atoum\telemetry;
 
 use mageekguy\atoum;
 use mageekguy\atoum\observable;
-use mageekguy\atoum\runner;
 use mageekguy\atoum\reports;
+use mageekguy\atoum\runner;
 
 class extension implements atoum\extension
 {
-	protected $runner;
-	protected $test;
+    protected $runner;
+    protected $test;
 
-	public function __construct(atoum\configurator $configurator = null)
-	{
-		if ($configurator)
-		{
-			$configurator->getScript()->setDefaultReportFactory(function($script) {
-				$report = new reports\realtime\cli();
-				$report->addWriter($script->getOutputWriter());
+    public function __construct(atoum\configurator $configurator = null)
+    {
+        if ($configurator) {
+            $configurator->getScript()->setDefaultReportFactory(function ($script) {
+                $report = new reports\realtime\cli();
+                $report->addWriter($script->getOutputWriter());
 
-				return $report;
-			});
+                return $report;
+            });
 
-			$parser = $configurator->getScript()->getArgumentsParser();
+            $parser = $configurator->getScript()->getArgumentsParser();
 
-			$handler = function($script, $argument, $values) {
-				$script->getRunner()->addTestsFromDirectory(dirname(__DIR__) . '/tests/units/classes');
-			};
+            $handler = function ($script, $argument, $values) {
+                $script->getRunner()->addTestsFromDirectory(dirname(__DIR__) . '/tests/units/classes');
+            };
 
-			$parser
-				->addHandler($handler, array('--test-ext'))
-				->addHandler($handler, array('--test-it'))
-			;
-		}
-	}
+            $parser
+                ->addHandler($handler, ['--test-ext'])
+                ->addHandler($handler, ['--test-it'])
+            ;
+        }
+    }
 
-	public function addToRunner(runner $runner)
-	{
-		$runner->addExtension($this);
+    public function addToRunner(runner $runner)
+    {
+        $runner->addExtension($this);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function setRunner(runner $runner)
-	{
-		$this->runner = $runner;
+    public function setRunner(runner $runner)
+    {
+        $this->runner = $runner;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getRunner()
-	{
-		return $this->runner;
-	}
+    public function getRunner()
+    {
+        return $this->runner;
+    }
 
-	public function setTest(atoum\test $test)
-	{
-		$this->test = $test;
+    public function setTest(atoum\test $test)
+    {
+        $this->test = $test;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getTest()
-	{
-		return $this->test;
-	}
+    public function getTest()
+    {
+        return $this->test;
+    }
 
-	public function handleEvent($event, observable $observable)
-	{
-		return $this;
-	}
+    public function handleEvent($event, observable $observable)
+    {
+        return $this;
+    }
 }
--- configuration.php
+++ configuration.php
@@ -4,9 +4,9 @@ use mageekguy\atoum;
 use mageekguy\atoum\scripts;
 
 if (defined('mageekguy\atoum\scripts\runner') === true) {
-	scripts\runner::addConfigurationCallable(function(atoum\configurator $script, atoum\runner $runner) {
-		$extension = new atoum\telemetry\extension($script);
+    scripts\runner::addConfigurationCallable(function (atoum\configurator $script, atoum\runner $runner) {
+        $extension = new atoum\telemetry\extension($script);
 
-		$extension->addToRunner($runner);
-	});
+        $extension->addToRunner($runner);
+    });
 }
